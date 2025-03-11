d65f478ee9c416e2664517347b40750230ee352b
Ldoppea <yannick.chiron@gmail.com>
2025-03-11 18:41:35 +0100

    feat: TEMP hashmap

--- android/src/main/java/com/margelo/nitro/bridgewebview/StaticMessages.java
+++ android/src/main/java/com/margelo/nitro/bridgewebview/StaticMessages.java
@@ -3,7 +3,10 @@ package com.margelo.nitro.bridgewebview;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StaticMessages {
-  public static List<String> Messages = new ArrayList<String>(Arrays.asList("sup1", "sup2", "sup3"));
+  // public static List<String> Messages = new ArrayList<String>(Arrays.asList("sup1", "sup2", "sup3"));
+  public static Map<String, String> Messages = new HashMap<>();
 }
