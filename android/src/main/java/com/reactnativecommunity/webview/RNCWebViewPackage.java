399127a89c30dba8603bc7345298fdf6244b1acd
Ldoppea <yannick.chiron@gmail.com>
2025-03-11 16:21:38 +0100

    feat: TEMP fix build

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewPackage.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewPackage.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.margelo.nitro.bridgewebview;
+import com.margelo.nitro.bridgewebview.NitroBridgeWebviewOnLoad;
 
 public class RNCWebViewPackage extends TurboReactPackage {
 
